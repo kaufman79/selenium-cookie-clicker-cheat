b72f13acc03ae0f13bdfdcbf984748755c4f6601
kaufman79 <kaufman79@gmail.com>
2024-07-23 12:21:18 -0400

    scrape python website for events

---
+++ testing_3.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# keep Chrome open after program finishes by configuring option and passing that as arg below
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get("https://python.org")
+
+# note that with the CSS_SELECTOR, with each space we are going
+# deeper into the hierarchy - sim to how XPath does it.
+event_times = driver.find_elements(By.CSS_SELECTOR,
+                                   value=".event-widget time")
+event_names = driver.find_elements(By.CSS_SELECTOR,
+                                   value=".event-widget li a")
+
+# make dict
+events = {}
+# write into dict
+for n in range(len(event_times)):
+    events[n] = {
+        "time": event_times[n].text,
+        "name": event_names[n].text
+    }
+
+print(events)
+
+driver.quit()
